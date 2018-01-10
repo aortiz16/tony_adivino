9cce123d5f9402e1582ea35ba4c1da9de01d311b
aortiz16 <aortiz16@gmail.com>
2018-01-10 21:10:38 +0100

    ORIGINAL refacTORIZAR

---
+++ Refactor/Listasimple/src/listasimple/Listasimple.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package listasimple;
+
+/**
+ *
+ * @author aortiz16
+ */
+public class EntradaBlogAO{
+	/*Clase para manejar las entradas de un blog.
+	 * La clase se creó el 21/3/1999 por Ana López.
+	 * Esta es la versión 2.3
+	 */
+	//Modificar.
+	public class EntradaBlog+INICIALES {
+		//separador es el ccarácter que separa ENTRADA DE del
+		//nombre del autor
+		
+		public static char separador=':';
+		private int id;
+		private String texto;
+		private String autor;
+		
+		//Constructor de la clase. recibe el número de entrada, el nombre del autor
+		//de la entrada y el texto que contiene la entrada. Si el número de entrada
+		//es negativo, lanza una excepción.
+		public EntradaBlog(int id,String autor,String texto)throws IllegalArgumentException{
+			if(id<=0) throw new IllegalArgumentException("El id no puede ser negativo");
+			this.id=id;
+			this.autor=autor;
+			this.texto=texto;
+		}
+		@Override
+		public String toString(){
+			String cad="";
+			cad+="\nENTRADA DE"+separador+autor;
+			cad+="\n "+texto;
+			return cad;
+		}
+		
+		//Devuelve el número de la entrada
+		public int getId(){
+			return this.id;
+		}
+		
+		//devuelve el texto completo de la entrada
+		public String getTexto(){
+			return this.texto;
+		}
+		
+		//devuelve el nombre del autor de la entrada en mayúsculas
+		public String getAutor(){
+			return this.autor.toUpperCase();
+		}
+		
+		//devuelve el nombre del autor. Ya no se usa. Mejor ver getAutor
+		public String devuelveAutor(){
+			return this.autor;
+		}
+		
+		//No tiene porqué tener argumentos.
+		public static void main(String[] args) {
+	                //Modificar.
+			EntradaBlog+INICIALES e1=new EntradaBlog+INICIALES (-3,"ana","Últimas noticias, está disponible BixBy 2.0");
+			System.out.println(e1);
+		}
+	}	
+}
+
+
+public class Listasimple {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
